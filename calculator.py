14171a7f04ebd98f09ddbbb79627b9786506f225
Tareef-debugger <tareeftoufique@gmail.com>
2025-08-25 18:58:20 +0600

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,13 @@
+
+def add(x,y):
+    return x+y
+def subtract(x,y):
+    return x-y
+def multiply(x,y):
+    return x*y
+def division(x,y):
+    return x/y
+print(add(3,5))
+print(subtract(3,5))
+print(multiply(3,5))
+print(division(3,5))
---
+++ intro.py
@@ -0,0 +1,8 @@
+def intro(name):
+    print("Hello, good evening I am ",name )
+user_name = input("Enter your name")
+intro(user_name)
+def iage(age1):
+    print("Hello, good evening I am ",age1,"years old" )
+user = input("Enter your age")
+iage(user)
